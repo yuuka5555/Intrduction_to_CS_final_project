7d19335bbcc3ba0b7766c588e75ed6c6da66eda9
Phantom0174 <darrenhsiou@gmail.com>
2023-05-25 00:13:07 +0800

    add music for squirrel chara.

--- src/final_project/Main.java
+++ src/final_project/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application{
 	public void start(Stage stage) throws Exception {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/scene/menu.fxml"));
 		Parent root = loader.load();
-		Scene scene = new Scene(root);				
+		Scene scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
 	}
