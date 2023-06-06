15c000c02eb16c3a4d11433b102c69c8d4e52413
yuuka5555 <zyoujia19@gmail.com>
2023-06-06 20:34:33 +0800

    66666666

--- src/scene/EndScene.java
+++ src/scene/EndScene.java
@@ -17,7 +17,7 @@ public class EndScene {
 	
 	public void returnToMenu(ActionEvent e) throws IOException {
 		Config.m.playClick("src/music/click.mp3");
-		Config.mc.play("src/music/menu.m4a");
+		Config.mc.play("src/music/menu.mp3");
 		root = FXMLLoader.load(getClass().getResource("menu.fxml"));
 		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
 		scene = new Scene(root);				
